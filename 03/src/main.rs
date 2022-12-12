3e6e5f521e75d31999de0e56a2cded3dde016d39
alexander.nordenstrom@gmail.com <alexander.nordenstromgmail.com>
2022-12-12 13:19:19 +0100

    Day 3 WIP

--- 03/src/main.rs
+++ 03/src/main.rs
@@ -1,16 +1,26 @@
+const OFFSET_UPPERCASE: u8 = 38;
+const OFFSET_LOWERCASE: u8 = 96;
+
 fn main() {
     let input = ::std::fs::read_to_string("input.txt").unwrap();
     let rucksacks = input.split("\r\n").collect::<Vec<&str>>();
-    let mut matches: Vec<char> = vec![];
+    let mut matches: Vec<u32> = vec![];
 
     for rucksack in rucksacks {
         let (first_compartment, second_compartment) = rucksack.split_at(rucksack.len() / 2);
+        println!("{:?} {:?}", first_compartment, second_compartment);
         for current_item in first_compartment.chars() {
             if second_compartment.contains(current_item) {
-                matches.push(current_item);
+                let offset: u32;
+                if current_item.is_uppercase() {
+                    offset = OFFSET_UPPERCASE as u32;
+                }
+                else {
+                    offset = OFFSET_LOWERCASE as u32;
+                }
+                matches.push((current_item as u32) - offset);
             }
         }
     }
-
-    println!("{:?}", matches);
+    println!("Part one: {}", matches.iter().sum::<u32>());
 }
